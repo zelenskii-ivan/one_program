ab789f06f1cda47a8f9895c8f1e94d382ec09651
FIRST_NAME LAST_NAMEgit config --global user.name FIRST_NAME <you@example.com>
2022-09-14 18:56:05 +0300

    Exemple003_Sum2

--- Exemple003_Sum/Program.cs
+++ Exemple003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next (1, 10);// 1 2 3 4 ... 9
+Console.WriteLine (numberA);
+int numberB = new Random().Next (1,10);
 int result = numberA + numberB;
 Console.WriteLine(numberA + numberB);
